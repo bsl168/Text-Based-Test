6333c66c9df0d85e2a529849afc5fe89a28b25af
bsl168 <32686236+bsl168@users.noreply.github.com>
2017-10-10 21:14:56 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -15,21 +15,18 @@ namespace HelloWorld
         {
             string CharacterName = "John Doe";
 
-            Console.WriteLine("Adventure!");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("It is the end of summer, you hear the cicadas chirping as you get out of your taxi.");
-            Console.WriteLine("Before you stands an old decrepit old house. The roof seems to be poorly maintained.");
+            Console.WriteLine("Adventure!\n\n" );
+            Console.WriteLine("It is the end of summer, you hear the cicadas chirping as you get out of your taxi.\n");
+            Console.WriteLine("Before you stands an old decrepit old house. The roof seems to be poorly maintained.\n");
             Console.WriteLine("\"I'm really not sure why you are here errrr...\" says the cab driver who seems to be at a loss with what to call you");
-            Console.WriteLine("You're probably never going to meet him again, but it's rude to not introduce yourself");
-            CharacterName = Console.ReadLine();
-            Console.WriteLine("The cab driver gives you a look like this is completely pointless \"Goodluck " +CharacterName+ " we're never meeting again.\" ");
-            Console.WriteLine("You stare as the taxi drives off, leaving you alone with the house. You don't know what his problem is but you're never meeting them again");
-            Console.WriteLine("There doesn't seem to be anything else nearby except for a mailbox");
+            Console.WriteLine("You're probably never going to meet him again, but it's rude to not introduce yourself\n\n");
+            CharacterName = Console.ReadLine(); //Fix blank entries being a valid entry for CharacterName
             Console.WriteLine("");
-            Console.WriteLine(" \"Hmmm...\" you think to yourself.");
-            Console.WriteLine("");
-            Console.WriteLine("What next?");
+            Console.WriteLine($"The cab driver gives you a look like this is completely pointless \"Goodluck  {CharacterName}! we're never meeting again.\"\n\n ");
+            Console.WriteLine("You stare as the taxi drives off, leaving you alone with the house. \nYou don't know what his problem is but you're never meeting them again");
+            Console.WriteLine("There doesn't seem to be anything else nearby except for a mailbox\n\n");
+            Console.WriteLine(" \"Hmmm...\" you think to yourself.\n");
+            Console.WriteLine("What next?\n");
             Console.Read();    
         }
     }
